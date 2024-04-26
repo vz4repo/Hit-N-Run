fa777e8e3e21b507fd7a03449806b17e066c0a3a
n90425 <102747591+n90425@users.noreply.github.com>
2024-04-26 20:11:45 +0900

    feat::[중섭] 주문 읽기
    * order- mapper,dto,dao,service
    
    * order 읽기
    
    * 중간 commit

---
+++ src/main/java/com/homerunball/ord/service/OrdService.java
@@ -0,0 +1,19 @@
+package com.homerunball.ord.service;
+
+import com.homerunball.ord.domain.OrdDto;
+
+import java.util.List;
+
+public interface OrdService {
+    int getcount() throws Exception;
+
+    int remove(String od_id, String c_id) throws Exception;
+
+    OrdDto read(String od_id, String c_id) throws Exception;
+
+    int writer(OrdDto ord) throws Exception;
+
+    List<OrdDto> getList() throws Exception;
+
+    int modify(OrdDto ord) throws Exception;
+}
