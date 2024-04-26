017d42a263957aebbcea8e6f9e21d8b90166831b
greensalttt <163237578+greensalttt@users.noreply.github.com>
2024-04-26 16:23:57 +0900

    feat::[민욱] 회원가입
    * homerunball cust update
    
    * after merge

---
+++ src/main/java/com/homerunball/DBConnectionTest2.java
@@ -0,0 +1,22 @@
+package com.homerunball;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+public class DBConnectionTest2 {
+    public static void main(String[] args) throws Exception {
+
+        ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
+        DataSource ds = ac.getBean(DataSource.class);
+
+        Connection conn = ds.getConnection();
+
+        System.out.println("conn = " + conn);
+    }
+}
+
