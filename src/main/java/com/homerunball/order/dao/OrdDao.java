cf9526d338b03c40f188951ef12eae0bbde66bb8
dani Kim <rlaeksl0124@naver.com>
2024-05-07 18:36:30 +0900

    feat::[다니] Cart , Order - User session 변경건 수정
    * Cart Delete
    * Cart delete & update...
    * cart update controller
    * folder 이동
    * cart Read Update Delete
    * cart update ajax
    * DB update
    * cart jquery use
    * Order TDD & Mapper update
    * cust PR Before

--- src/main/java/com/homerunball/order/dao/OrdDao.java
+++ src/main/java/com/homerunball/order/dao/OrdDao.java
@@ -1,33 +1,28 @@
 package com.homerunball.order.dao;
 
-import com.homerunball.customer.domain.CustDto;
 import com.homerunball.order.domain.OrdDto;
-import com.homerunball.order.domain.OrderDetDto;
 
 import java.util.List;
 
 public interface OrdDao {
-
-
-
-
-    String getcid() throws Exception;
     int count() throws Exception; // T selectOne(String statement)
 
-    int delete(Integer od_id, String c_id) throws Exception; // int delete(String statement, Object parameter)
+    int delete(String od_id, String c_id) throws Exception; // int delete(String statement, Object parameter)
 
-    int deleteAll() throws Exception; // int delete(String statement)
+    int deleteAll() throws Exception;
 
-    int insert(OrdDto ord) throws Exception;
+    int insert(OrdDto dto) throws Exception;
 
-    int update(OrdDto ord) throws Exception;
+    int update(OrdDto dto) throws Exception;
 
 //    static OrdDto select(String od_id) throws Exception; // T selectOne(String statement, Object parameter)
 
     List<OrdDto> selectAll() throws Exception; // List<E> selectList(String statement)
 
-    List<OrdDto> selectOdId(Integer od_id) throws Exception;
+    List<OrdDto> selectOdId(String od_id) throws Exception;
 
-    OrdDto select(Integer od_id, String c_id) throws Exception; // T selectOne(String statement, Object parameter)
+    List<OrdDto> selectCid(String c_id) throws Exception;
+
+    OrdDto select(String od_id, String c_id) throws Exception; // T selectOne(String statement, Object parameter)
 
 }
