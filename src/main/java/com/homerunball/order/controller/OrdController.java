a014d00b771c3e3edd034aaf097d601954072653
n90425 <102747591+n90425@users.noreply.github.com>
2024-05-16 10:33:51 +0900

    refactor::[중섭]order테이블 insert 개선
    * order- mapper,dto,dao,service
    * order 읽기
    git commit -a -m tmp commit
    *order Detail dao Tdd
    * 주문240503
    * re-commit 240503
    * 주문번호 생성
    * OrdAndStk Mapper & JSP
    * 24.05.13 최종
    * 24.05.14 주문내역 출력,주문테이블 insert중
    * 24.05.15 주문테이블 insert 완성

--- src/main/java/com/homerunball/order/controller/OrdController.java
+++ src/main/java/com/homerunball/order/controller/OrdController.java
@@ -4,6 +4,7 @@ import com.homerunball.cart.dao.CartDao;
 import com.homerunball.cart.domain.CartDto;
 import com.homerunball.order.dao.OrdDao;
 import com.homerunball.order.dao.OrderAndStkDao;
+import com.homerunball.order.dao.OrderDetDao;
 import com.homerunball.order.domain.OrdAndStkDto;
 import com.homerunball.order.domain.OrdDto;
 import com.homerunball.order.domain.OrderDetDto;
@@ -19,7 +20,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 @Controller
@@ -32,6 +35,8 @@ public class OrdController {
     @Autowired
     CartDao cartDao;
     @Autowired
+    OrderDetDao orderdetDao;
+    @Autowired
     OrderAndStkDao orderAndStkDao;
 
 
@@ -41,25 +46,76 @@ public class OrdController {
             return "redirect:/login?toURL="+request.getRequestURI();
         int c_id = (int)session.getAttribute("c_id");
         try {
+//            List<CartDto> list = cartDao.selectUser(c_id);
+            List<CartDto> list =cartDao.getStk(c_id);
+           /* List<OrdAndStkDto> stkList = orderAndStkDao.getcartItem(c_id);*/
 
-            List<CartDto> list = cartDao.selectUser(c_id);
-         //String pd_name, int sls_prc, int cart_cnt, int c_id
+            System.out.println("list = " +list);
 
-            List<OrdAndStkDto> stkList = orderAndStkDao.getcartItem(c_id);
-//            int totalPrice = OrdAndStkDto.OrdAndStkTotalPrice();
-//            OrdAndStkDto ordDto = new OrdAndStkDto();
-//            ordDao.insert(ord);
-            System.out.println(stkList);
-
+            OrderDetDto ord_det = new OrderDetDto();
+            OrdDto ord = new OrdDto();
 
 
-            m.addAttribute("list",list);
-            m.addAttribute("stkList", stkList);
+            /*장바구니에서 data 가져와서 order_det 테이블에 insert */
+            for (CartDto cart : list){
+                ord_det.setPd_id(cart.getPd_id());
+                ord_det.setPd_clsf_cd(cart.getPd_clsf_code());
+                ord_det.setPd_name(cart.getPd_name());
+                ord_det.setSls_prc(cart.getSls_prc());
+                ord_det.setOd_qty(cart.getCart_cnt());
+                ord_det.setC_id(cart.getC_id());
+
+                orderdetDao.insert(ord_det);
+            }
+
+
+            int totalpd_qty = 0;
+            int totalqty = 0;
+            int totalamount = 0;
+            int totalrtlamount = 0;
+
+            /*중복 안돼는 set을 만든다*/
+            Set<String> od_pd_qtyid = new HashSet<>();
+
+            for (CartDto cart : list) {
+                /*제품 가지 수*/
+                od_pd_qtyid.add(cart.getPd_id());     /*set에 상품 id를 넣어서 for문 돌려줌*/
+
+                /*주문 총 수량 구하기*/
+                totalqty += cart.getCart_cnt();
+
+                /*주문 총 금액 구하기*/
+                int itemtotal = cart.getSls_prc() * cart.getCart_cnt();
+                totalamount += itemtotal;
+
+                /*주문 총 소비자가 구하기*/
+                totalrtlamount += cart.getRtl_prc();
+            }
+
+            /*상품 id를 담은 set(od_pd_qtyid)의 사이즈를 담아줌*/
+            totalpd_qty = od_pd_qtyid.size();
+
+
+            ord.setC_id(ord_det.getC_id());
+            ord.setOd_pd_qty(totalpd_qty);
+            ord.setOd_tot_qty(totalqty);
+            ord.setOd_pay_amt(totalamount);
+            ord.setRtl_prc(totalrtlamount);
+
+            ordDao.insert(ord);
+
+            System.out.println("ord_det.getC_id()" +ord_det.getC_id());
+            System.out.println(ord);
+
+            m.addAttribute("list", list);
+            /*m.addAttribute("stkList", stkList);*/
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         return "order";
     }
+
     private boolean loginCheck(HttpServletRequest request){
         HttpSession session = request.getSession();
         return session.getAttribute("c_id") != null;
