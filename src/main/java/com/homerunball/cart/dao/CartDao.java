b80739a88465421d92940b7ac115a5cade38d052
dani Kim <rlaeksl0124@naver.com>
2024-05-17 17:23:16 +0900

    stk에서 cart Image 불러오기 (#43)
    * Cart Mapper update
    * stk에서 cart Image 불러오기

--- src/main/java/com/homerunball/cart/dao/CartDao.java
+++ src/main/java/com/homerunball/cart/dao/CartDao.java
@@ -1,9 +1,11 @@
 package com.homerunball.cart.dao;
 
+import com.homerunball.admin.product.domain.ProductDto;
 import com.homerunball.cart.domain.CartDto;
 import com.homerunball.customer.domain.CustDto;
 
 import java.util.List;
+import java.util.Map;
 
 public interface CartDao {
     List<CustDto> getcustAll() throws Exception;
@@ -21,6 +23,8 @@ public interface CartDao {
     boolean exists(CartDto cart) throws Exception;
 
     int insert(CartDto cart) throws Exception;
+
+    CartDto selectCart(int c_id, String pd_id, String pd_clsf_code) throws Exception;
 
     int update(CartDto cart) throws Exception;
 
