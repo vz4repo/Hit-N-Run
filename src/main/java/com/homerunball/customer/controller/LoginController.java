68034f227dfb4508bb70f4a265c0b5d822427735
n90425 <n90425@naver.com>
2024-05-20 16:20:25 +0900

    a

--- src/main/java/com/homerunball/customer/controller/LoginController.java
+++ src/main/java/com/homerunball/customer/controller/LoginController.java
@@ -87,9 +87,9 @@ public class LoginController {
             }
 
 
-//            if (!(custDto.getC_pwd().equals(c_pwd))) {
-//                return false;
-//            }
+           /* if (!(custDto.getC_pwd().equals(c_pwd))) {
+                return false;
+            }*/
 
             /*로그인 성공시 updateLoginDate 메서드 실행*/
             custDao.updateLoginDt(c_email);
