f71d551792c5689645db63cade498cbc086fe7c2
vz4repo <50684258+vz4repo@users.noreply.github.com>
2024-05-22 11:09:42 +0900

    feat::[혁락]주문/배송/결제 화면에 배송지 출력, 배송지 선택
    * css 변경 및 배송지 기능 추가
    * order.jsp 선택된 배송정보 출력
    * order.jsp 배송지 수정/삭제버튼 삭제
    * 주문상세 날짜범위로 검색

--- src/main/java/com/homerunball/order/dao/OrderDetDao.java
+++ src/main/java/com/homerunball/order/dao/OrderDetDao.java
@@ -3,24 +3,28 @@ package com.homerunball.order.dao;
 import com.homerunball.customer.domain.CustDto;
 import com.homerunball.order.domain.OrderDetDto;
 
+import com.homerunball.payment.domain.PaymentDto;
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Map;
 
 public interface OrderDetDao {
 
 
-    /* 리스트 수를 센다*/
-    int count() throws Exception;
+  /* 리스트 수를 센다*/
+  int count() throws Exception;
 
-    int delete(int od_det_seqnum, BigInteger od_id, int c_id) throws Exception;
+  int delete(int od_det_seqnum, BigInteger od_id, int c_id) throws Exception;
 
-    int deleteAll() throws Exception;
+  int deleteAll() throws Exception;
 
-    List<OrderDetDto> select(int c_id) throws Exception;
+  List<OrderDetDto> select(int c_id) throws Exception;
 
-    List<OrderDetDto> selectAll() throws Exception;
+  List<OrderDetDto> selectAll() throws Exception;
 
-    int insert(OrderDetDto ord_det) throws Exception;
+  int insert(OrderDetDto ord_det) throws Exception;
 
-    int update(OrderDetDto ord_det) throws Exception;
+  int update(OrderDetDto ord_det) throws Exception;
+
+  List<OrderDetDto> selectPaymentHistoryWithDateRange(Map<String, Object> localDateMap);
 }
